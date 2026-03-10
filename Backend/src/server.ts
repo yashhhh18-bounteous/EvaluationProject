a43a7dc86134029337f3e3d8082d97e671e72ce7
Yash Premchandra Jha <yash.jha@bounteous.com>
2026-03-10 18:30:30 +0530

    fixing cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -13,7 +13,8 @@ app.use(cookieParser())
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://evaluation-project-seven.vercel.app"],
+    
     credentials: true
   })
 )
